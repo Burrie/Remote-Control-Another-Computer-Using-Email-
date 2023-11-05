1d92399d9267dacfa03c195e053792a6f2bc3094
HandsomeBoy699 <nguyenhuugiahieu1@gmail.com>
2023-11-05 23:23:19 +0700

    Update MailChecker.py

--- MailChecker.py
+++ MailChecker.py
@@ -10,8 +10,6 @@ imap = imaplib.IMAP4_SSL(host = server, port = 993)
     
 imap.login(account, password)
 
-imap.select("Inbox")
-
 #Check mail and implement
 def CheckAndDo(cmd):
     if(cmd == 'list applications'):
@@ -30,25 +28,26 @@ def CheckAndDo(cmd):
 #Select unseen message in Inbox to read
 cmd = 'start'
 while cmd != 'quit':
+    imap.select("Inbox")
     res, mailIds = imap.search(None, '(UNSEEN)')
+    print(res)
 
     #Try to read email
-    try:
-        for id in mailIds[0].decode().split():
-            res, mailData = imap.fetch(id, '(RFC822)')
-            message = email.message_from_string(mailData[0][1].decode())
+    for id in mailIds[0].decode().split():
+        res, mailData = imap.fetch(id, '(RFC822)')
+        message = email.message_from_string(mailData[0][1].decode())
 
-            #Get message
-            for part in message.walk():
-                if(part.get_content_type() == 'text/plain'):
-                    cmd = part.as_string().splitlines()[-1]
-                    CheckAndDo(cmd = cmd)
+        #Get message
+        for part in message.walk():
+            if(part.get_content_type() == 'text/plain'):
+                cmd = part.as_string().splitlines()[-1]
+                CheckAndDo(cmd = cmd)
                     
-    except Exception as e:
-        print("Error type: {}, Error: {}".format(type(e).__name__, e))
-        
-    time.sleep(1)
+    
+    imap.close()
+    
+    time.sleep(0.75)
 
-imap.close()
+print("Byee")
 
 imap.logout()
